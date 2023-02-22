e1b839971c02e0a74c7102a59083a283156ba6c8
dm-evstafiev <d.evstafiev@crystals.ru>
2023-02-23 02:21:14 +0300

    -

--- src/main/java/com/example/SpringDemoBot/config/BotConfig.java
+++ src/main/java/com/example/SpringDemoBot/config/BotConfig.java
@@ -1,7 +1,15 @@
 package com.example.SpringDemoBot.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
 public class BotConfig {
 
+    @Value("${bot.name}")
     String botName;
+    @Value("${bot.token}")
     String token;
 }
